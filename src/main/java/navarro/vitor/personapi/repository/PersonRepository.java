46e7b5ea233d545070da0f550d67dfca4165e490
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-12 19:04:56 -0300

    add person repository

---
+++ src/main/java/navarro/vitor/personapi/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package navarro.vitor.personapi.repository;
+
+import navarro.vitor.personapi.entity.Person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
