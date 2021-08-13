3673a68aca1d3fcf211738460d9148dacb5172f9
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-13 13:22:32 -0300

    wip change application name

--- src/main/java/navarro/vitor/personapi/PersonapiApplication.java
+++ src/main/java/navarro/vitor/personapi/PersonapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonApiApplication {
+public class PersonapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonApiApplication.class, args);
+		SpringApplication.run(PersonapiApplication.class, args);
 		System.out.println("Everything's fine!");
 	}
 
