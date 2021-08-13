e501e9365fece8c3ae8770540d135735ad0ea1a4
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-13 13:10:15 -0300

    change application name

--- src/main/java/navarro/vitor/personapi/PersonApiApplication.java
+++ src/main/java/navarro/vitor/personapi/PersonApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonapiApplication {
+public class PersonApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonapiApplication.class, args);
+		SpringApplication.run(PersonApiApplication.class, args);
 		System.out.println("Everything's fine!");
 	}
 
