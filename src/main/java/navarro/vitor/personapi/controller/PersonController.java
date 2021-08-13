a915bd07f32a29966cb58aa9b584b2bac35c8595
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-13 11:40:20 -0300

    add response DTO

--- src/main/java/navarro/vitor/personapi/controller/PersonController.java
+++ src/main/java/navarro/vitor/personapi/controller/PersonController.java
@@ -1,16 +1,27 @@
 package navarro.vitor.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import navarro.vitor.personapi.dto.response.MessageResponseDTO;
+import navarro.vitor.personapi.entity.Person;
+import navarro.vitor.personapi.repository.PersonRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-  @GetMapping
-  public String getBook() {
+  private PersonRepository personRepository;
 
-    return "API test!";
+  @Autowired
+  public PersonController(PersonRepository personRepository) {
+    this.personRepository = personRepository;
   }
+
+  @PostMapping
+  public MessageResponseDTO createPerson(@RequestBody Person person) {
+    Person savedPerson = personRepository.save(person);
+    return MessageResponseDTO.builder().message("created person with id " + savedPerson.getId()).build();
+  }
+
 }
---
+++ src/main/java/navarro/vitor/personapi/dto/response/MessageResponseDTO.java
@@ -0,0 +1,10 @@
+package navarro.vitor.personapi.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+  private String message;
+}
