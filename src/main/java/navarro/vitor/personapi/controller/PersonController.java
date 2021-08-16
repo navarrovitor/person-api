29d7feb3bb6405447ade87750f6c52f7958d4133
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-16 19:24:32 -0300

    add delete person functionality, add verify if exists function

--- src/main/java/navarro/vitor/personapi/controller/PersonController.java
+++ src/main/java/navarro/vitor/personapi/controller/PersonController.java
@@ -40,4 +40,10 @@ public class PersonController {
     return personService.findById(id);
   }
 
+  @DeleteMapping("/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+    personService.delete(id);
+  }
+
 }
