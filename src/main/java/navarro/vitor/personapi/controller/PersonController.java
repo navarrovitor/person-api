472feabcab31f75a8ccd443e1fe0ebe909a0ada3
vitor navarro <vitor.san.nav.nexus@gmail.com>
2021-08-16 20:49:03 -0300

    add all args constructor with autowired function on controller and service

--- src/main/java/navarro/vitor/personapi/controller/PersonController.java
+++ src/main/java/navarro/vitor/personapi/controller/PersonController.java
@@ -13,16 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
   private PersonService personService;
-
-  @Autowired
-  public PersonController(PersonService personService) {
-    this.personService = personService;
-  }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/navarro/vitor/personapi/service/PersonService.java
+++ src/main/java/navarro/vitor/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
 import navarro.vitor.personapi.dto.request.PersonDTO;
 import navarro.vitor.personapi.dto.response.MessageResponseDTO;
 import navarro.vitor.personapi.entity.Person;
@@ -14,17 +15,12 @@ import navarro.vitor.personapi.mapper.PersonMapper;
 import navarro.vitor.personapi.repository.PersonRepository;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
   private PersonRepository personRepository;
 
   private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-  @Autowired
-  public PersonService(PersonRepository personRepository) {
-
-    this.personRepository = personRepository;
-  }
 
   public MessageResponseDTO createPerson(PersonDTO personDTO) {
     Person personToSave = personMapper.toModel(personDTO);
@@ -52,7 +48,6 @@ public class PersonService {
     verifyIfExists(id);
 
     Person personToUpdate = personMapper.toModel(personDTO);
-    personToUpdate.setId(id);
 
     Person updatedPerson = personRepository.save(personToUpdate);
     return createMessageResponse(updatedPerson.getId(), "updated person with ID ");
